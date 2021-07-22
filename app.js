acadf85c9cb95e251fe3fb8f1e249b2b5730380e
fullcounterr <fullcountra@gmail.com>
2021-07-22 10:39:56 +0700

    Fix navbar (especially for mobile device)

--- app.js
+++ app.js
@@ -16,7 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/css/bootstrap/', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+app.use('/js/bootstrap/', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
